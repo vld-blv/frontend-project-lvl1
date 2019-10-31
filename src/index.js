de7724d108a145f0aee1752ae5ed87314d1cf761
Vlad Balanov <vladislav.balanov@gmail.com>
2019-10-31 22:17:58 +0300

    added game engine

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import readlineSync from 'readline-sync';
 
-export let name = '';
+let playerName = '';
 
-export const greeting = () => {
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
-  name = actual;
+const greeting = () => {
+  const name = readlineSync.question('\nMay I have your name? ');
+  console.log(`Hello, ${name}!\n`);
+  playerName = name;
 };
 
+const gameProcess = (description, game) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(description);
+  greeting();
+  for (let i = 0; i < 3; i += 1) {
+    const correctAnswer = game();
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== String(correctAnswer)) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      break;
+    }
+    console.log('Correct!\n');
+    if (i === 2) console.log(`Congratulations, ${playerName}!`);
+  }
+};
+
+export default gameProcess;
