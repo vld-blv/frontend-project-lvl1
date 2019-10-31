39ba7e6ff3c55b8588781d43ff7bca25d31c58c9
Vlad Balanov <vladislav.balanov@gmail.com>
2019-10-31 22:46:03 +0300

    added GCD game

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import gameProcess from '..';
+import { description, greatestCommonDivisor } from '../games/gcd';
+
+gameProcess(description, greatestCommonDivisor);
---
+++ src/games/gcd.js
@@ -0,0 +1,16 @@
+
+import getRandomInt from '../random';
+
+export const description = 'Find the greatest common divisor of given numbers.';
+
+export const greatestCommonDivisor = () => {
+  const a = getRandomInt(1, 40);
+  const b = getRandomInt(1, 50);
+  const gcd = (n, m) => {
+    if (m === 0) return n;
+    return gcd(m, n % m);
+  };
+  const correctAnswer = gcd(a, b);
+  console.log(`Question: ${a} ${b}`);
+  return correctAnswer;
+};
