dc87d759561a92f78b346768c75306e7b7b9fc19
Vlad Balanov <vladislav.balanov@gmail.com>
2019-11-06 06:53:36 +0300

    edited isEven func

--- src/games/even.js
+++ src/games/even.js
@@ -5,9 +5,9 @@ import getRandomInt from '../random';
 export const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 export const isEvenNumber = () => {
-  const isEven = (x) => (x % 2 === 0 ? 'yes' : 'no');
+  const isEven = (x) => x % 2 === 0;
   const number = getRandomInt(1, 100);
   console.log(`Question: ${number}`);
-  const correctAnswer = isEven(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
   return correctAnswer;
 };
