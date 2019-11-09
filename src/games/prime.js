5030a63457b66545eaea769fc0936baa28f67a6f
Vlad Balanov <vladislav.balanov@gmail.com>
2019-11-09 17:39:36 +0300

    edited isPrime func according to CQS

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,11 +6,17 @@ import getRandomInt from '../random';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrimeNumber = (n) => {
-  for (let i = 1; i <= n / 2; i += 1) {
-    if (n % i === 0) return 'no';
+const isPrime = (n) => {
+  if (n <= 1) return false;
+  for (let i = 2; i < n / 2; i += 1) {
+    if (n % i === 0) return false;
   }
-  return 'yes';
+  return true;
+};
+
+const isPrimeNumber = (num) => {
+  if (isPrime(num)) return 'yes';
+  return 'no';
 };
 
 const gameQuestionAndAnswer = () => {
