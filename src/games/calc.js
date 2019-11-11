dbe22650f1a6449a18e9014cf322d2d77e5ecb59
Vlad Balanov <vladislav.balanov@gmail.com>
2019-11-11 22:16:48 +0300

    replaced substr() with substring()

--- src/games/calc.js
+++ src/games/calc.js
@@ -11,7 +11,8 @@ const operators = '*+-';
 const getGameQuestionAndAnswer = () => {
   const a = getRandomInt(1, 40);
   const b = getRandomInt(1, 50);
-  const randomOperator = operators.substr(getRandomInt(0, operators.length - 1), 1);
+  const getRandomOperator = getRandomInt(0, operators.length - 1);
+  const randomOperator = operators.substring(getRandomOperator, getRandomOperator + 1);
   const question = `${a} ${randomOperator} ${b}`;
   let correctAnswer = 0;
 
