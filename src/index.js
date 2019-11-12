85ab94b4940a52482174eb39fcab185306d6e3f7
Vlad Balanov <vladislav.balanov@gmail.com>
2019-11-12 20:05:09 +0300

    added roundsCount const

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@
 import { car, cdr } from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
 
 const playGame = (description, gameQuestionAndAnswer) => {
   console.log('Welcome to the Brain Games!');
   console.log(description);
   const playerName = readlineSync.question('\nMay I have your name? ');
   console.log(`Hello, ${playerName}!\n`);
-  for (let i = 0; i < 3; i += 1) {
+  for (let i = 0; i < roundsCount; i += 1) {
     const questionAndAnswer = gameQuestionAndAnswer();
     const gameQuestion = car(questionAndAnswer);
     const correctAnswer = cdr(questionAndAnswer);
